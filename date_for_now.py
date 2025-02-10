063af616cd0543616d0ef49e2ee64ab8bf9a04fb
Enotkiller <enothal0@gmail.com>
2025-02-10 13:41:36 +0200

    by nasosik

--- date_for_now.py
+++ date_for_now.py
@@ -14,13 +14,25 @@ class date_now:
         data = self.time_return().split(":")
         time = float(f"{data[0]}.{data[1]}")
         if time >= 8.0 and time <= 9.50:
-            return 1
-        elif time >= 9.5 and time <= 11.20:
-            return 2
+            if time >= 8.0 and time <= 8.30:
+                return -1
+            else:
+                return 1
+        elif time >= 9.50 and time <= 11.20:
+            if time >= 9.50 and time <= 11.0:
+                return -2
+            else:
+                return 2
         elif time >= 11.20 and time <= 13.20:
-            return 3
+            if time >= 11.20 and time <= 12.0:
+                return -3
+            else:
+                return 3
         elif time >= 13.20 and time <= 14.50:
-            return 3
+            if time >= 13.20 and time <= 13.30:
+                return -4
+            else:
+                return 4
 
 if __name__ == '__main__':
     pass
