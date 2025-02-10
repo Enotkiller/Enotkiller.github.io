261b3b4d55359d69279a809fa43d364975bd77bf
recurtis <sherbinindima16@gmail.com>
2025-02-10 13:40:19 +0200

    cfsdf

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #файл пидора
-import aiogram
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+import logging
 import asyncio
+bot = Bot(token = '7080600577:AAHIKU7SrX8XmQrcnZlb5fLtVjOtAdHt-NU')
+dp = Dispatcher()
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer('qq')
+async def main():
+    await dp.start_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
