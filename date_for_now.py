d02048d4adac8472ee183a17f9c933cb39f18dd2
Enotkiller <enothal0@gmail.com>
2025-02-10 14:21:01 +0200

    by nasosik

--- date_for_now.py
+++ date_for_now.py
@@ -33,6 +33,55 @@ class date_now:
                 return -4
             else:
                 return 4
+    def para_return(self):
+        cal = {
+            1 : {
+                1 : "Биология",
+                2 : "Биология",
+                3 : "Химия",
+                4 : "Оборона Украины"
+            },
+            2 : {
+                1 : "Оборона Украины",
+                2 : "Физра",
+                3 : "Английский",
+                4 : None
+            },
+            3 : {
+                1 : "Математика",
+                2 : "Всемирная история",
+                2.5 : "Химия",
+                3 : "Математика",
+                4 : "Зарубежная литература"
+            },
+            4 : {
+                1 : "История Украины",
+                2 : "География",
+                3 : "Английский",
+                3.5 : "Физика",
+                4 : "Физика"
+            },
+            5 : {
+                1 : "Информатика",
+                2 : "Украинский язык",
+                3 : "Украинская литература",
+                4 : None
+            }
+        }
+        parameters = []
+        quest = self.quest_return()
+        data = self.date_return()
+        if data <= 5:
+            if quest < 0:
+                parameters.append("перемена")
+            else:
+                parameters.append("Урок")
+            for i in range(1, 4 + 1):
+                if (quest == i or quest == i * -1):
+                    parameters.append(cal.get(data).get(i))
+            return parameters
+        else:
+            return "Выходные"
 
 if __name__ == '__main__':
-    print(date_now().quest_return())
+    print(date_now().para_return())
