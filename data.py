7ab3bbd3d8126eb5ff96a06096574de97fc50472
Enotkiller <enothal0@gmail.com>
2025-02-12 09:52:30 +0200

    by nasosik huesosik

--- data.py
+++ data.py
@@ -37,20 +37,20 @@ class data:
             }
         }
         self.url = {
-    "Биология" : "https://meet.google.com/eed-rtog-srd?authuser=4&hs=179",
-    "Химия" : "https://meet.google.com/cut-rvao-zbt?authuser=4&hs=179",
-    "Оборона Украины" : "https://meet.google.com/uqv-omtr-bwx?authuser=4&hs=179",
-    "Физра" : "https://meet.google.com/mxt-wvmr-gny?authuser=4&hs=179",
-    "Английский" : "1 Группа: https://meet.google.com/svs-snwo-tfm?authuser=4&hs=179\n2 Группа: https://meet.google.com/hdq-zamk-pka",
+    "Биология" : "https://meet.google.com/eed-rtog-srd9",
+    "Химия" : "https://meet.google.com/cut-rvao-zbt",
+    "Оборона Украины" : "https://meet.google",
+    "Физра" : "https://meet.google.com/mxt-wvmr-gny",
+    "Английский" : "1 Группа: https://meet.google.com/svs-snwo-tfm\n2 Группа: https://meet.google.com/hdq-zamk-pka",
     "Математика" : "https://us02web.zoom.us/j/84351065107?pwd=WOrdR7On7gbcrPynf178H0A9FW3M3k.1",
-    "Зарубежная литература" : "https://meet.google.com/mnx-uxwk-wgo?authuser=4&hs=179",
-    "История Украины" : "https://meet.google.com/zww-totu-kva?authuser=4&hs=179",
-    "География" : "https://meet.google.com/gyc-nwne-nvi?authuser=4&hs=179",
-    "Физика" : "https://meet.google.com/znu-moir-atb?authuser=4&hs=179",
-    "Информатика" : "https://meet.google.com/kwt-xdbj-kdq?authuser=4&hs=179",
-    "Украинский язык" : "https://meet.google.com/jkz-wqku-xyh?authuser=4&hs=179",
-    "Украинская литература" : "https://meet.google.com/nem-mhnk-ghe?authuser=4&hs=179",
-    "Всемирная история" : "https://meet.google.com/qay-vxca-jfw?authuser=4&hs=179"
+    "Зарубежная литература" : "https://meet.google.com/mnx-uxwk-wgo",
+    "История Украины" : "https://meet.google.com/zww-totu-kva",
+    "География" : "https://meet.google.com/gyc-nwne-nvi",
+    "Физика" : "https://meet.google.com/znu-moir-atb",
+    "Информатика" : "https://meet.google.com/kwt-xdbj-kdq",
+    "Украинский язык" : "https://meet.google.com/jkz-wqku-xyh",
+    "Украинская литература" : "https://meet.google.com/nem-mhnk-ghe",
+    "Всемирная история" : "https://meet.google.com/qay-vxca-jfw"
 }
         self.full_time = date_now().full_date_return()
         self.data = int(datetime.datetime.now().strftime("%d"))
