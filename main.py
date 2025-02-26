6087e92abdb028e3d70a1bc7ce4d6993635353db
Enotkiller <enothal0@gmail.com>
2025-02-26 08:30:27 +0200

    by nasosik huesosik

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 import logging
 import asyncio
 from data import data
@@ -17,7 +17,8 @@ bot = Bot(token = '7080600577:AAHIKU7SrX8XmQrcnZlb5fLtVjOtAdHt-NU')
 dp = Dispatcher()
 days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
 @dp.message(Command("para"))
-async def cmd_start(message: Message):
+async def cmd_start(message: Message, command: CommandObject):
+    args = command.args
     print(message.from_user.username, message.from_user.id)
     if db.data_weekly() <= 5 and db.time_float() <= 14.50 and message.from_user.id != 5376094724:
         await message.answer(f"{days[db.data_weekly() - 1]}\nТекущее время: {db.time()}\n{"Текущая" if not db.peremena() else "Будет"} пара: {db.para()}\nСтатус: {(("Отпустили" if db.otmena_now() == 1 else "Перемена" if db.peremena() else ("Идёт") if message.from_user.id != 7250542929 else "Не идёт, а летит") if db.para() != "Пары нет" else "Ничего нет")}\nСсылка: {db.get_url()}")
@@ -25,6 +26,11 @@ async def cmd_start(message: Message):
         await message.answer(f"Картель вызван...\nВремя: {db.time()}\nОписание к заказу: лиж бы все, но не {db.para()}")
     else:
         await message.answer("Какие уроки челл.")
+@dp.message(Command("test"))
+async def test(message: Message, command: CommandObject):
+    args = command.args
+    await message.answer(args)
+
 
 @dp.message(Command("papa"))
 async def papa(message: Message):
