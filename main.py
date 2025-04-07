d022a5e23909ca4d4c10a081b6ccc245e5ba8eec
Enotkiller <enothal0@gmail.com>
2025-04-07 09:10:27 +0300

    by nasosik huesosik

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ import asyncio
 from code import *
 
 if __name__ == '__main__':
-    # asyncio.run(bot_aiogram().start())
-    print(bot_aiogram().get_time_str())
+    asyncio.run(bot_aiogram().start())
