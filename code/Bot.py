ee22857cb96c425258e288febf9e44b0997d47e4
Enotkiller <enothal0@gmail.com>
2025-04-19 20:05:30 +0300

    by nasosik huesosik

--- code/Bot.py
+++ code/Bot.py
@@ -85,7 +85,7 @@ class bot_aiogram(system):
         Сдесь происходит отправка сообщение перед парой в чат который задан в chat_id
         """
 
-        if not self.get_day_weekly_now() in [6, 7] and self.get_pair_now() != None:
+        if not self.get_day_weekly_now() in [6, 7] and self.get_pair_now() is None:
             try:
                 self.read_file()
                 username = self.username
@@ -100,7 +100,7 @@ class bot_aiogram(system):
                 print(f"Сообщение отправлено в {datetime.now().strftime('%H:%M:%S')}")
             except Exception as e:
                 print(f"Ошибка при отправке сообщения: {e}")
-        elif self.get_pair_now() == None:
+        elif self.get_pair_now() is None:
             await self.bot.send_message(chat_id=self.chat_id, text="Пары нема, все расходимся.")
     async def scheduler(self, target_times: list):
         """
