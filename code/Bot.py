52be3471d7e71dab00c6c05402efc8049cd4bace
PPqewqqweqq <mrpusya05@gmail.com>
2025-06-04 08:59:24 +0300

    by nasosik huesosik

--- code/Bot.py
+++ code/Bot.py
@@ -35,8 +35,8 @@ class bot_aiogram(system):
             print(message.from_user.username, message.from_user.id)
             if self.get_day_weekly_now() <= 5 and self.get_time_float() <= 14.50:
                 await message.answer(f"""{self.days[self.get_day_weekly_now() - 1]}\nТекущее время: {self.get_time_str()}
-{"Текущая" if not self.get_pair_number_type(self.get_pair_number_now()) else "Будет"} пара: {self.get_pair_now()}
-Статус: {"Отпустили" if self.get_cancellation() == True else "Перемена" if self.get_pair_number_type(self.get_pair_number_now()) else "Идёт"}
+{'Текущая' if not self.get_pair_number_type(self.get_pair_number_now()) else 'Будет'} пара: {self.get_pair_now()}
+Статус: {'Отпустили' if self.get_cancellation() == True else 'Перемена' if self.get_pair_number_type(self.get_pair_number_now()) else 'Идёт'}
 Ссылка: {self.get_url_pair()}""")
             else:
                 await message.answer("Какие уроки челл.")
