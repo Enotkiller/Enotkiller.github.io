b071b924bd19d406234715bd30b256b1349c2b97
Enotkiller <enothal0@gmail.com>
2025-10-25 11:25:14 +0300

    last

--- code/System.py
+++ code/System.py
@@ -24,7 +24,7 @@ class System:
             print_debug("System", "[main]Initialized.[/main]")
 
         self.database = (
-            DataBase.DataBase(_debug=_debug) if _database is None else _database
+            DataBase.DataBase(_debug = _debug) if _database is None else _database
         )
         self.cancellation = []
 
@@ -32,7 +32,7 @@ class System:
         self,
         day_week: int = None,
         number_lesson: int = None,
-        type=None,
+        type = None,
         _debug: bool = True,
     ) -> tuple[str, str] | tuple[None, None]:
         """
@@ -56,11 +56,12 @@ class System:
                 "System",
                 f"Get lesson: day: [main]{day_week}[/main], number lesson: [main]{int(number_lesson)}[/main], type: [main]{type}[/main]",
             )
-        if int(number_lesson) > 0 and int(number_lesson) <= int(
-            self.database.get_max_lesson_in_days(_debug=False)
+
+        if int(number_lesson) > 0 and int(number_lesson) <=  int(
+            self.database.get_max_lesson_in_days(_debug = False)
         ):
             ids, lessons = self.database.get_lesson_for_date(
-                day_week, int(number_lesson), _debug=False
+                day_week, int(number_lesson), _debug = False
             )
 
             if lessons and ids:
@@ -101,14 +102,14 @@ class System:
                 "System",
                 f"Get lesson for date: day: [main]{day}[/main], number lesson: [main]{number_lesson}[/main]",
             )
-        date = self.get_day_isoweekly(year=year, mounth=mounth, day=day)
+        date = self.get_day_isoweekly(year = year, mounth = mounth, day = day)
         ids = self.database.get_id_lesson_for_date(
-            day=date, number_lesson=number_lesson, _debug=False
+            day = date, number_lesson = number_lesson, _debug = False
         )
         _, lessons = self.database.get_lesson_for_date(
-            day=date, number_lesson=number_lesson, _id_lessons=ids, _debug=False
+            day = date, number_lesson = number_lesson, _id_lessons = ids, _debug = False
         )
-        type = self.get_week_type(_day=day, _mounth=mounth, _year=year, _debug=False)
+        type = self.get_week_type(_day = day, _mounth = mounth, _year = year, _debug = False)
 
         if lessons and ids:
             lessons.reverse()
@@ -119,7 +120,7 @@ class System:
         return None, None
 
     def get_lesson_now(
-        self, _day=None, _number_lesson=None, _type=None, _debug: bool = True
+        self, _day = None, _number_lesson = None, _type = None, _debug: bool = True
     ) -> tuple[str, str] | tuple[None, None]:
         """
 
@@ -133,19 +134,19 @@ class System:
             tuple[str] : id и пара или ничего.
         """
 
-        day = self.get_day_isoweekday_now(_debug=False) if _day is None else _day
+        day = self.get_day_isoweekday_now(_debug = False) if _day is None else _day
         number_lesson = (
             self.get_lesson_number_now_without_type()
             if _number_lesson is None
             else _number_lesson
         )
         ids = self.database.get_id_lesson_for_date(
-            day=day, number_lesson=number_lesson, _debug=False
+            day = day, number_lesson = number_lesson, _debug = False
         )
         _, lessons = self.database.get_lesson_for_date(
-            day=day, number_lesson=number_lesson, _id_lessons=ids, _debug=False
+            day = day, number_lesson = number_lesson, _id_lessons = ids, _debug = False
         )
-        type = self.get_week_type(_debug=False) if _type is None else _type
+        type = self.get_week_type(_debug = False) if _type is None else _type
 
         if _debug:
             print_debug(
@@ -162,7 +163,7 @@ class System:
         return None, None
 
     def get_url(
-        self, _day=None, _number_lesson=None, _type=None, _id=None, _debug: bool = True
+        self, _day = None, _number_lesson = None, _type = None, _id = None, _debug: bool = True
     ) -> str | None:
         """
 
@@ -177,17 +178,17 @@ class System:
         """
 
         if _id:
-            return self.database.get_url_for_id(id=_id, _debug=False)
+            return self.database.get_url_for_id(id = _id, _debug = False)
 
-        day = self.get_day_isoweekday_now(_debug=False) if _day is None else _day
+        day = self.get_day_isoweekday_now(_debug = False) if _day is None else _day
         number_lesson = (
-            self.get_lesson_number_now_without_type(_debug=False)
+            self.get_lesson_number_now_without_type(_debug = False)
             if _number_lesson is None
             else _number_lesson
         )
-        type = self.get_week_type(_debug=False) if _type is None else _type
+        type = self.get_week_type(_debug = False) if _type is None else _type
         id, lesson = self.get_lesson(
-            day_week=day, number_lesson=number_lesson, type=type, _debug=False
+            day_week = day, number_lesson = number_lesson, type = type, _debug = False
         )
 
         if _debug:
@@ -197,7 +198,7 @@ class System:
             )
 
         if lesson:
-            return self.database.get_url_for_id(id=id, _debug=False)
+            return self.database.get_url_for_id(id = id, _debug = False)
         return None
 
     def recess_now(self, _debug: bool = True) -> bool:
@@ -306,10 +307,10 @@ class System:
             (
                 mounth
                 if mounth
-                else self.database.get_data_week_type(_debug=False)["month"]
+                else self.database.get_data_week_type(_debug = False)["month"]
             ),
-            day if day else self.database.get_data_week_type(_debug=False)["day"],
-            year if year else self.database.get_data_week_type(_debug=False)["year"],
+            day if day else self.database.get_data_week_type(_debug = False)["day"],
+            year if year else self.database.get_data_week_type(_debug = False)["year"],
         )
         when_mounth, when_day, when_year = (
             np.uint(when_mounth),
@@ -320,11 +321,11 @@ class System:
         day = int(datetime.datetime.now().strftime("%d")) if not _day else _day
         year = int(datetime.datetime.now().strftime("20%y")) if not _year else _year
 
-        max_type = self.database.get_number_of_weeks_types(_debug=False) - 1
+        max_type = self.database.get_number_of_weeks_types(_debug = False) - 1
         type = (
             type
             if type is not None
-            else self.database.get_data_week_type(_debug=False)["type"]
+            else self.database.get_data_week_type(_debug = False)["type"]
         )
         days = np.uint16(0)
 
@@ -336,26 +337,26 @@ class System:
         )
 
         if (
-            self.get_day_isoweekly(year=when_year, mounth=when_mounth, day=when_day)
-            == 1
+            self.get_day_isoweekly(year = when_year, mounth = when_mounth, day = when_day)
+             ==  1
         ):
-            type += 1
+            type +=  1
             if type > max_type:
                 type = 0
 
         for i in range(mounth - when_mounth + 1):
             for j in range(1, calendar.monthrange(year, i + when_mounth)[1] + 1):
-                if i == 0 and j <= when_day - 1:
+                if i  == 0 and j <=  when_day - 1:
                     pass
 
                 else:
                     if (
-                        self.get_day_isoweekly(year=year, mounth=when_mounth + i, day=j)
-                        == 1
+                        self.get_day_isoweekly(year = year, mounth = when_mounth + i, day = j)
+                         ==  1
                     ):
-                        days += 1
+                        days +=  1
 
-                    if i + when_mounth == mounth and j >= day:
+                    if i + when_mounth  ==  mounth and j >= day:
                         break
 
         for i in range(days):
@@ -396,27 +397,27 @@ class System:
             np.int8 : номер пары.
         """
 
-        time_now = self.get_time_float(_debug=False)
-        times = self.database.get_times(_debug=False)
+        time_now = self.get_time_float(_debug = False)
+        times = self.database.get_times(_debug = False)
         for number_lesson in times:
             if (
                 self.get_time_float(
-                    _time=times[number_lesson]["start_recess"], _debug=False
+                    _time = times[number_lesson]["start_recess"], _debug = False
                 )
                 <= time_now
                 <= self.get_time_float(
-                    _time=times[number_lesson]["start_lessons"], _debug=False
+                    _time = times[number_lesson]["start_lessons"], _debug = False
                 )
             ):
                 return np.int8(-int(number_lesson))
 
             if (
                 self.get_time_float(
-                    _time=times[number_lesson]["start_lessons"], _debug=False
+                    _time = times[number_lesson]["start_lessons"], _debug = False
                 )
                 <= time_now
                 <= self.get_time_float(
-                    _time=times[number_lesson]["end_lessons"], _debug=False
+                    _time = times[number_lesson]["end_lessons"], _debug = False
                 )
             ):
                 return np.int8(int(number_lesson))
@@ -437,11 +438,11 @@ class System:
         if _debug:
             print_debug("System", "Get [main]time now[/main].")
 
-        time_zone = ZoneInfo(self.database.get_time_zone(_debug=False))
+        time_zone = ZoneInfo(self.database.get_time_zone(_debug = False))
         time_now = datetime.datetime.now(time_zone)
         return time_now
 
-    def get_time_str(self, _time=None, _debug: bool = True) -> str:
+    def get_time_str(self, _time = None, _debug: bool = True) -> str:
         """
 
         Получение времени сейчас в str.
@@ -455,10 +456,10 @@ class System:
         if _debug:
             print_debug("System", "Get [main]time[/main] in [main]time str[/main].")
 
-        time = self.get_time_now(_debug=False) if _time is None else _time
+        time = self.get_time_now(_debug = False) if _time is None else _time
         return f"{(str('0') + str(time.hour)) if int(time.hour) < 10 else str(time.hour)}:{(str('0') + str(time.minute)) if int(time.minute) < 10 else str(time.minute)}"
 
-    def get_time_float(self, _time=None, _debug: bool = True) -> float:
+    def get_time_float(self, _time = None, _debug: bool = True) -> float:
         """
 
         Получение времени сейчас в float.
@@ -474,11 +475,11 @@ class System:
             print_debug("System", "Get [main]time[/main] in [main]time float[/main].")
 
         if _time is None:
-            time = self.get_time_now(_debug=False)
+            time = self.get_time_now(_debug = False)
             return float(float(time.hour) + (float(time.minute) / 100))
 
         else:
-            if type(_time) is str and _time != "" and ":" in _time:
+            if type(_time) is str and _time !=  "" and ":" in _time:
                 return float(_time.split(":")[0] + "." + _time.split(":")[1])
 
     def get_lesson_number_now_without_type(self, _debug: bool = True) -> np.uint8:
@@ -515,8 +516,8 @@ class System:
         if mass is None:
             self.cancellation.append(
                 [
-                    self.get_lesson_number_now_without_type(_debug=False),
-                    self.get_day_isoweekday_now(_debug=False),
+                    self.get_lesson_number_now_without_type(_debug = False),
+                    self.get_day_isoweekday_now(_debug = False),
                 ]
             )
 
@@ -533,7 +534,7 @@ class System:
             if type(mass) is list or type(mass) is tuple:
                 for i in mass:
                     self.cancellation.append(
-                        [i, self.get_day_isoweekday_now(_debug=False)]
+                        [i, self.get_day_isoweekday_now(_debug = False)]
                     )
                     
                 if _debug:
@@ -559,11 +560,11 @@ class System:
         delete = []
         for i in range(len(self.cancellation)):
             cancel = self.cancellation[i]
-            if cancel[1] is self.get_day_isoweekday_now(_debug=False):
-                if cancel[0] < self.get_day_isoweekday_now(_debug=False):
+            if cancel[1] is self.get_day_isoweekday_now(_debug = False):
+                if cancel[0] < self.get_day_isoweekday_now(_debug = False):
                     delete.append(self.cancellation[i])
 
-            elif cancel[1] < self.get_day_isoweekday_now(_debug=False):
+            elif cancel[1] < self.get_day_isoweekday_now(_debug = False):
                 delete.append(self.cancellation[i])
 
         for i in delete:
@@ -571,8 +572,8 @@ class System:
 
         for i in range(len(self.cancellation)):
             if self.cancellation[i][0] == self.get_lesson_number_now_without_type(
-                _debug=False
-            ) and self.cancellation[i][1] == self.get_day_isoweekday_now(_debug=False):
+                _debug = False
+            ) and self.cancellation[i][1] == self.get_day_isoweekday_now(_debug = False):
                 return True
 
         return False
